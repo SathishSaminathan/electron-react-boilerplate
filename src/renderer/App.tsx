69224eb0d60cb49866f5f2dfdf259c238c53c9d8
Sathish Saminathan <Sathishcse1975@gmail.com>
2022-05-20 08:30:38 +0530

    initial commit

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,15 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import Login from 'pages/login';
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
+import icon from 'assets/icon.svg';
 import './App.css';
 
 const Hello = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="Hello">
@@ -11,21 +18,23 @@ const Hello = () => {
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
         <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
+          // href="https://electron-react-boilerplate.js.org/"
+          // target="_blank"
+          onClick={() => navigate('/login')}
+          rel="noreferrer noopener"
         >
           <button type="button">
             <span role="img" aria-label="books">
               📚
             </span>
-            Read our docs
+            Read our doc
           </button>
         </a>
         <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
+          onClick={() => navigate('/login')}
+          // href="https://github.com/sponsors/electron-react-boilerplate"
+          // target="_blank"
+          rel="noreferrer noopener"
         >
           <button type="button">
             <span role="img" aria-label="books">
@@ -44,6 +53,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Hello />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
